bc11ca24029d17fb7c67a8202daf3ac7d1b5ff8a
shadin khan <13307041@iubat.edu>
2025-09-19 19:32:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Countries from './component/Countries/Countries'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Countries></Countries>
+      
+      
+    </>
+  )
+}
+
+export default App
