175f33dd00c3450ca1f207e4980a39ef429db70d
shadin khan <13307041@iubat.edu>
2025-09-20 00:47:37 +0600

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
 import Countries from './component/Countries/Countries'
 import './App.css'
+import { Suspense } from 'react'
+
+const countriesPromise = fetch("https://openapi.programming-hero.com/api/all").then((res) =>res.json())
 
 function App() {
   
 
   return (
     <>
-     
-      <Countries></Countries>
+      <Suspense fallback={<p>Nadir Vai on the gooo...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+     </Suspense>
+      
       
       
     </>
